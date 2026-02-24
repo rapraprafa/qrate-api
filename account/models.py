8cea4d3dbfd3db5f8a8a6bb25c44f2f9228ff883
Rafael Balayo II <rbalayo@famgenix.com>
2026-02-25 07:00:42 +0800

    gitignore

--- account/models.py
+++ account/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Account(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.user.username
